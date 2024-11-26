ba45a1dce544971954fbd46a6ef6e84e2f155c13
Simone Mine <simonminekoza@gmail.com>
2024-11-26 16:57:47 +0100

    seed-on-process

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,6 +3,4 @@ class Route < ApplicationRecord
   belongs_to :user
   has_many :runs
   has_many :steps
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,22 +12,25 @@ Route.destroy_all
 
 puts 'create steps'
 
-Step.create(longitude: )
+Step.create(longitude: 13.3958819, latitude: 52.5191864, position: 0, route: kreuzberg)
+Step.create(longitude: 13.3958819, latitude: 52.5191864, position: 0, route: kreuzberg)
+Step.create(longitude: 13.3958819, latitude: 52.5191864, position: 0, route: kreuzberg)
+Step.create(longitude: 13.3958819, latitude: 52.5191864, position: 0, route: kreuzberg)
 
 puts 'create routes'
 
-Route.create(name: "Kreuzberg Park Loop", distance: 5, address: "Großbeerenstraße 32, 10965 Berlin",
+kreuzberg = Route.create(name: "Kreuzberg Park Loop", distance: 5,
              description: "A scenic loop through Kreuzberg’s lush parks, ideal for a relaxing run on sunny afternoons.")
-Route.create(name: "Prenzlauer Berg Pathway", distance: 10, address: "Kollwitzstraße 65, 10435 Berlin",
+Route.create(name: "Prenzlauer Berg Pathway", distance: 10,
              description: "Explore the charming streets and tree-lined paths of Prenzlauer Berg,
               perfect for crisp autumn mornings.")
-Route.create(name: "Charlottenburg Canal Run", distance: 25, address: "Spandauer Damm 20-24, 14059 Berlin",
+Route.create(name: "Charlottenburg Canal Run", distance: 25,
              description: "A tranquil route along the canal in Charlottenburg,
               great for peaceful evening jogs by the water")
-Route.create(name: "Mitte Skyline Sprint", distance: 15, address: "Alexanderplatz 1, 10178 Berlin",
+Route.create(name: "Mitte Skyline Sprint", distance: 15,
              description: "Dash through Berlin’s vibrant city center with
              views of iconic landmarks—best enjoyed during a clear, breezy day.")
-Route.create(name: "Tempelhof Track Trail", distance: 5, address: "Columbiadamm 84, 10965 Berlin",
+Route.create(name: "Tempelhof Track Trail", distance: 5,
              description: "Run on the historic Tempelhof airfield,
                a wide-open route perfect for windy or overcast days.")
 

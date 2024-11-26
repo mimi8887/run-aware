7838d5958ddf1bb3fff215fdfc464faae2793468
Simone Mine <simonminekoza@gmail.com>
2024-11-26 14:59:02 +0100

    seeds

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,5 +1,12 @@
 class RoutesController < ApplicationController
   def index
-    
+    @routes = Route.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @routes.geocoded.map do |route|
+      {
+        lat: route.latitude,
+        lng: route.longitude
+      }
+    end
   end
 end
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -27,5 +27,5 @@ Geocoder.configure(
   timeout: 5, # seconds
   lookup: :mapbox, # Defaults to :nominatim, which is less reliable but free
   api_key: ENV['MAPBOX_API_KEY'],
-  units: :km, # Defaults to miles (:mi)
+  units: :km,
 )

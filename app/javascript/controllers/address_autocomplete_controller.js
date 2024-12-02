e985de0b6586da1961314cd0020ef6dd20dc75f1
Simone Mine <simonminekoza@gmail.com>
2024-12-02 13:33:02 +0100

    search bar not working so much

---
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -0,0 +1,34 @@
+// app/javascript/controllers/address_autocomplete_controller.js
+import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+// Connects to data-controller="address-autocomplete"
+export default class extends Controller {
+  static values = { apiKey: String }
+  static targets = ["start_address", "end_address"]
+
+  connect() {
+    console.log('Hello')
+    this.geocoder = new MapboxGeocoder({
+      accessToken: this.apiKeyValue,
+      types: "country,region,place,postcode,locality,neighborhood,address",
+      countries: "DE"
+    })
+    this.geocoder.addTo(this.element)
+    this.geocoder.on("result", event => this.#setInputValue(event))
+    this.geocoder.on("clear", () => this.#clearInputValue())
+  }
+
+  #setInputValue(event) {
+    this.addressTarget.value = event.result["place_name"]
+    console.log('Hello')
+  }
+
+  #clearInputValue() {
+    this.addressTarget.value = ""
+  }
+
+  disconnect() {
+    this.geocoder.onRemove()
+  }
+}

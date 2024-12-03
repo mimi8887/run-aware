53d05e5dc535dc134eaa819ab9a5d1f97523f36f
Simone Mine <simonminekoza@gmail.com>
2024-12-03 17:52:44 +0100

    dropdown-search

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -24,11 +24,10 @@ class BookmarksController < ApplicationController
       @bookmark = Bookmark.new(bookmark_params)
       @bookmark.user = current_user
       @bookmark.route = @route
-      raise
        if @bookmark.save
-        redirect_to bookmarks_path, notice: 'Bookmark created successfuly.'
-      else
-        render :new, status: :unprocessable_entity
+          redirect_to bookmarks_path, notice: 'Bookmark created successfuly.'
+        else
+          render :new, status: :unprocessable_entity
       end
     else
       if params[:bookmark][:photos].present?
